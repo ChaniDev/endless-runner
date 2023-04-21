d1d52a0c42f24142c5c9ae6a7cc5dc09cb918ff1
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-04-21 14:03:35 +0530

    Update PlayerMovement.cs

--- Endless Runner/Assets/PlayerMovement.cs
+++ Endless Runner/Assets/PlayerMovement.cs
@@ -4,11 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    int currentLane = 0; 
+    [SerializeField] int currentLane = 0; 
+    [SerializeField] int selectedLane = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Left"))
+        {
+            Debug.Log("TEs");
+
+            switch(currentLane)
+            {
+                case 0:
+                    selectedLane = -1;
+                    break;
+
+                case -1:
+                    selectedLane = -1;
+                    break;
+
+                case +1:
+                    selectedLane = 0;
+                    break;
+            }
+
+            UpdatePlayerLocation();
+        }
+
+        if(Input.GetButtonDown("Right"))
+        {
+            Debug.Log("TEs");
+
+            switch(currentLane)
+            {
+                case 0:
+                    selectedLane = +1;
+                    break;
+
+                case -1:
+                    selectedLane = 0;
+                    break;
+
+                case +1:
+                    selectedLane = +1;
+                    break;
+            }
+
+            UpdatePlayerLocation();
+        }
+    }
+
+    void UpdatePlayerLocation()
+    {
+        if(selectedLane == currentLane)
+        {
+            return;
+        }
+
+        switch(selectedLane)
+        {
+            case -1:
+                transform.Translate(new Vector3(4,0,0) * Time.deltaTime * 100f);
+                currentLane = selectedLane;
+                break;
+        }
     }
 }
